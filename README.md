49646c081497ae6ce63c36df8ca9254c218dc8ea
Reid McIlroy-Young <reid@reidmcy.com>
2018-01-03 04:31:58 -0600

    Need these too

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# lucem-illud
+# lucem_illud
 light from text, a collection of helper functions for the class
--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
 from setuptools import setup, find_packages
 import re
+import os.path
 
-with open('lucem_ilud/__init__.py') as f:
-    versionString = re.search(r"__version__ = '(.+)'", f.read()).group(1)
+versionString = '0.0.1'
 
 if __name__ == '__main__':
     setup(name='lucem_ilud',
         version = versionString,
-        author="Reid McIlroy-Young",
+        author="James Evans, Reid McIlroy-Young",
+        author_email = "reidmcy@uchicago.edu",
+        license = 'GPL',
+        url="https://github.com/Computational-Content-Analysis-2018/lucem-illud",
+        #download_url = "https://github.com/networks-lab/metaknowledge/archive/{}.tar.gz".format(versionString),
         packages = find_packages(),
         install_requires = [
                 'numpy',
-        ]
+        ],
+        classifiers = [
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Framework :: Jupyter',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Sociology',
+        'Topic :: Text Processing',
+        ],
     )
