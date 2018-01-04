dbd7465977347563a71bed387857439404b312d7
Reid McIlroy-Young <reid@reidmcy.com>
2018-01-04 11:39:44 -0600

    Bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import re
 import os.path
 
-versionString = '0.0.3'
+versionString = '0.0.4'
 
 if __name__ == '__main__':
     setup(name='lucem_illud',
