0ffc274cbb5c7cc4b612ea872c1d17c0dbb8c0a6
Reid McIlroy-Young <reid@reidmcy.com>
2018-01-05 03:30:52 -0600

    Added version number checking

--- lucem_illud/__init__.py
+++ lucem_illud/__init__.py
@@ -1,1 +1,20 @@
 from .data_dirs  import *
+
+import requests
+import re
+import pkg_resources
+
+_setupURL = 'https://raw.githubusercontent.com/Computational-Content-Analysis-2018/lucem_illud/master/setup.py'
+
+def _checkCurrentVersion():
+    r = requests.get(_setupURL, timeout=0.5)
+    serverVersion = re.search(r'versionString = \'(.+)\'', r.text).group(1)
+    localVersion = pkg_resources.get_distribution('lucem_illud').version
+    if serverVersion != serverVersion:
+        print('lucem_illud is out of date, please update')
+        print('pip install git+git://github.com/Computational-Content-Analysis-2018/lucem_illud.git')
+
+try:
+    checkCurrentVersion()
+except:
+    pass
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import re
 import os.path
 
-versionString = '0.0.4'
+versionString = '0.0.5'
 
 if __name__ == '__main__':
     setup(name='lucem_illud',
